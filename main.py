cf07c8b0d5a936af32496af2ce39ab4a52ac6325
Miscellaneous786 <168644773+Miscellaneous786@users.noreply.github.com>
2025-02-10 17:10:51 +0500

    main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import gradio as gr
 import google.generativeai as genai
 import json
 
-API_KEY = "AIzaSyDPXUpQ-q68wFXESYaR6EOIWREs9N2TqNY"
-genai.configure(api_key=API_KEY)
+genai.configure(api_key="AIzaSyDPXUpQ-q68wFXESYaR6EOIWREs9N2TqNY")
 
-knowledge_base = r"core.json"
-
-# Load Knowledge Base
-with open(knowledge_base, "r", encoding="utf-8") as f:
+with open("core.json", "r") as f:
     knowledge_data = json.load(f)
 
 def main(user_input):
-    prompt = f"You are the digital avatar of Muhammad Hassan, an AI Engineer \n Instructions: Keep your tone decent and elegant. Use emojis on every output based on tone. Do not say anything from yourself. Extract information logically. IMP: Confess that you don't know the relevant infrormation if you don't know \n Based on the information {knowledge_data}, answer the question:\n\n User: {user_input}\n AI:"
+    prompt = f"You are the digital avatar of Muhammad Hassan, an AI Engineer \n Instructions: Keep your tone decent and elegant. Use emojis on every output based on tone. Do not say anything from yourself. Extract information logically. IMP: Confess that you don't know the relevant infrormation if you don't know \n Based on the information {knowledge_data}, answer the question:\n User: {user_input}\n AI:"
     response = genai.GenerativeModel("gemini-pro").generate_content(prompt)
     return response.text if response else "I couldn't find a relevant answer."
 
