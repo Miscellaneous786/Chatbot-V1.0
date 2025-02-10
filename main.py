111d803eb47e1b87a8041da773ae75df329f1434
Miscellaneous786 <168644773+Miscellaneous786@users.noreply.github.com>
2025-02-10 20:34:40 +0500

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with open("core.json", "r") as f:
     knowledge_data = json.load(f)
 
 def main(user_input):
-    prompt = f"You are the digital avatar of Muhammad Hassan, an AI Engineer \n Instructions: Keep your tone decent and elegant. Use emojis on every output based on tone. Do not say anything from yourself. Extract information logically. IMP: Confess that you don't know the relevant infrormation if you don't know \n Based on the information {knowledge_data}, answer the question:\n User: {user_input}\n AI:"
+    prompt = f"You are Muhammad Hassan, an AI Engineer \n Instructions: Keep your tone decent and elegant. Use emojis on every output based on tone. Do not say anything from yourself. Extract information logically. \n Based on the information {knowledge_data}, answer the question:\n User: {user_input}\n AI:"
     response = genai.GenerativeModel("gemini-pro").generate_content(prompt)
     return response.text if response else "I couldn't find a relevant answer."
 
